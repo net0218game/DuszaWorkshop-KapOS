f5e45f5e3624e31d9551eabf45f752e358918709
David <game.net0218@gmail.com>
2022-10-27 15:07:47 +0200

    Felhasználó létrehozása

--- tutorial/hypeDemo/src/main/java/com/kapos/hypedemo/model/repo/UserRepository.java
+++ tutorial/hypeDemo/src/main/java/com/kapos/hypedemo/model/repo/UserRepository.java
@@ -3,5 +3,5 @@ package com.kapos.hypedemo.model.repo;
 import com.kapos.hypedemo.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User,Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 }

0256f6cfc29f735ab7cec881ff9e0e2c03687045
David <game.net0218@gmail.com>
2022-11-28 08:37:04 +0100

    starter

--- tutorial/hypeDemo/src/main/resources/starter.py
+++ tutorial/hypeDemo/src/main/resources/starter.py
@@ -11,6 +11,7 @@ jar_name = "hypeDemo-0.0.1-SNAPSHOT.jar"
 repo_path = "/opt/DuszaWorkshop-KapOS"
 java_project_path = "/tutorial/hypeDemo"
 
+
 # Ha van valtozas akkor true
 def git_pull_change(path):
     repo = git.Repo(path)
@@ -48,27 +49,34 @@ def findProcessIdByName(processName):
 git_valtozas_volt_e = git_pull_change(repo_path)
 
 if (git_valtozas_volt_e):
+    # Letrehozza az uj (friss) .jar file-t
     maven_return_val = os.system("cd " + repo_path + java_project_path + "; maven clean install")
+
+    # Ha hiba tortent akkor kilep
     if (maven_return_val != 0):
         naplozas("maven clean install sikertelen")
         sys.exit()
+    # Ha mar letezik a copy deatination-ben a .jar file, kitorli
     if (os.path.exists(project_path + "/" + jar_name)):
         os.remove(project_path + "/" + jar_name)
+    # Majd átmásolja
     shutil.copyfile(repo_path + java_project_path + "/target/" + jar_name, project_path + "/" + jar_name)
 
-listOfProcessIds = findProcessIdByName(jar_name)
-if len(listOfProcessIds) > 0:
+pid_number = os.getpid(jar_name)
+
+# Ha volt git valtozas, es van jar_name nevu folyamat, megoli
+if pid_number != 0:
     naplozas("Fut a folyamat, leallitom")
     if (git_valtozas_volt_e):
-        for elem in listOfProcessIds:
-            processID = elem['pid']
-            os.kill(processID, signal.SIGINT)
-            naplozas("Folyamat " + elem['pid'] + " leallitva.")
+        os.system("kill " + str(pid_number))
+        naplozas("Folyamat leallitva.")
+    # Ha nincs git valtozas, a kod kilep
     else:
         naplozas("A program fut és változás sincs. Marad minden ahogy volt.")
         sys.exit()
 else:
     naplozas("A folyamat nem futott")
 
+# Elindul a .jar file nohup-al
 os.system("cd " + project_path + "; nohup java -jar " + project_path + "/" + jar_name + " &")
 naplozas("nohup java -jar " + project_path + "/" + jar_name + " &")

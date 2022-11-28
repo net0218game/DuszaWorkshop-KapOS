cea70d76b7da8a8f71f04b8f412baabdb703d64e
David <game.net0218@gmail.com>
2022-11-28 08:06:06 +0100

    starter.py fix??

--- tutorial/hypeDemo/src/main/resources/starter.py
+++ tutorial/hypeDemo/src/main/resources/starter.py
@@ -11,6 +11,7 @@ jar_name = "hypeDemo-0.0.1-SNAPSHOT.jar"
 repo_path = "/opt/DuszaWorkshop-KapOS"
 java_project_path = "/tutorial/hypeDemo"
 
+# Ha van valtozas akkor true
 def git_pull_change(path):
     repo = git.Repo(path)
     current = repo.head.commit
@@ -24,50 +25,50 @@ def git_pull_change(path):
         naplozas("A repo valtozott.")
         return True
 
+
 def naplozas(szoveg):
     print(str(datetime.time) + "\t" + szoveg)
 
+
+# Processek megkeresese
 def findProcessIdByName(processName):
-    '''
-    Get a list of all the PIDs of a all the running process whose name contains
-    the given string processName
-    '''
     listOfProcessObjects = []
-    #Iterate over the all the running process
+    # Iterate over the all the running process
     for proc in psutil.process_iter():
-       try:
-           pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
-           # Check if process name contains the given name string.
-           if processName.lower() in pinfo['name'].lower() :
-               listOfProcessObjects.append(pinfo)
-       except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
-           pass
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
+            # Check if process name contains the given name string.
+            if processName.lower() in pinfo['name'].lower():
+                listOfProcessObjects.append(pinfo)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
     return listOfProcessObjects
 
 
-git_valtozas_volt_e=git_pull_change(repo_path)
+git_valtozas_volt_e = git_pull_change(repo_path)
 
 if (git_valtozas_volt_e):
-    maven_return_val = os.system("cd " + repo_path + java_project_path+"; maven clean install")
+    maven_return_val = os.system("cd " + repo_path + java_project_path + "; maven clean install")
     if (maven_return_val != 0):
         naplozas("maven clean install sikertelen")
         sys.exit()
-    if(os.path.exists(project_path + "/" + jar_name)):
+    if (os.path.exists(project_path + "/" + jar_name)):
         os.remove(project_path + "/" + jar_name)
-    shutil.copyfile(repo_path+java_project_path+"/target/"+jar_name, project_path+"/"+jar_name)
+    shutil.copyfile(repo_path + java_project_path + "/target/" + jar_name, project_path + "/" + jar_name)
 
 listOfProcessIds = findProcessIdByName(jar_name)
 if len(listOfProcessIds) > 0:
-   naplozas("Fut a folyamat, leallitom")
-   if(git_valtozas_volt_e):
-       for elem in listOfProcessIds:
-           processID = elem['pid']
-           os.kill(processID, signal.SIGINT)
-   else:
+    naplozas("Fut a folyamat, leallitom")
+    if (git_valtozas_volt_e):
+        for elem in listOfProcessIds:
+            processID = elem['pid']
+            os.kill(processID, signal.SIGINT)
+            naplozas("Folyamat " + elem['pid'] + " leallitva.")
+    else:
         naplozas("A program fut és változás sincs. Marad minden ahogy volt.")
-        exit()
-else :
-   naplozas("A folyamat nem futott")
+        sys.exit()
+else:
+    naplozas("A folyamat nem futott")
 
-os.system("cd " + project_path + "; nohup java -jar "+ project_path+"/"+jar_name +" &")
-naplozas("nohup java -jar "+ project_path+"/"+jar_name +" &")
+os.system("cd " + project_path + "; nohup java -jar " + project_path + "/" + jar_name + " &")
+naplozas("nohup java -jar " + project_path + "/" + jar_name + " &")
